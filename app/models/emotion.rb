6b30ad9b8b4ed5d938f76f6fcc2caf4376b1ba53
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-06-02 00:02:48 +0900

    commonization_with_scope

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,3 +1,10 @@
 class Emotion < ApplicationRecord
     belongs_to :user, optional: true
+
+    scope :with_weekly_data, -> { where("updated_at": 1.week.ago.beginning_of_day..Time.zone.now.end_of_day) }
+    scope :with_monthly_data, -> { where("updated_at": 1.month.ago.beginning_of_day..Time.zone.now.end_of_day) }
+    scope :search_with_id, ->(content_id) { where("user_id": content_id) }
+    scope :search_with_feeling, ->(feeling) { pluck(feeling) }
 end
+
+
